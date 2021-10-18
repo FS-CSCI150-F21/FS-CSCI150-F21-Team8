db091eec7af7be3ac83c0ed450b40669ce8e5eb8
daniel-adam-ce <danieladamce@gmail.com>
2021-10-17 19:27:09 -0700

    Updated user model
    Added some missing data types to user schema. Also added some default and required statements.

--- server/models/user.js
+++ server/models/user.js
@@ -4,17 +4,34 @@ import mongoose from 'mongoose'
 // not entirely sure how sensitive information is going to be stored yet
 // (it will be hashed, but not sure if that is of String datatype)
 const userSchema = mongoose.Schema({
-    email: String,
-    password: String, // see above note
-    displayName: {
+    email: {
         type: String,
+        lowercase: true,
+        required: true,
+    }, // need to add some method of validation (ensure that email is an email, ensure that email is not duplicate)
+    password: {
+        type: String,
+        required: true,
+    }, // see above note
+    displayName: String,
+    description: String,
+    joinDate: {
+        type: Date,
         default: Date.now()
     },
-    joinDate: Date,
-    description: String,
-    profilePicture: Blob, // not sure if correct
-    currentAuctions: Array, // may need adjusting later,
-    currentBids: Array,
-})
+    profilePicture: Buffer, // not sure if correct datatype
+    currentAuctions: {
+        type: Array,
+        of: [mongoose.Schema.ObjectId],
+        default: []
+    }, // not sure if correct declaration
+    currentBids: {
+        type: Array,
+        of: Number,
+        default: []
+    },
+}, { collection: 'users'})
+
+const UserData = mongoose.model('user', userSchema);
 
 export default UserData;
