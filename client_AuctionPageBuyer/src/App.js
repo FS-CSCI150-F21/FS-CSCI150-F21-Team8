4a1adbc417c91ccfabb6700718514575f6311c61
Brian Alvarez Irineo <irineobrian17@gmail.com>
2021-10-21 08:36:49 -0700

    updated buyer's auction page wireframe. still need to format and connect to db

---
+++ client_AuctionPageBuyer/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Body from "./Components/Body/Body";
+import Header from "./Components/Header/Header"
+import './index.css'
+
+function App() {
+    return (
+      <div className="App">
+          <Header />
+          <Body />
+      </div>
+    )
+  }
+  
+  export default App;
