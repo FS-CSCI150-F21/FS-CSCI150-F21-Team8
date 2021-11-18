e6bd57557fa439445662c301b6ac68ea5386b397
Daniel Adam <danieladamce@gmail.com>
2021-11-18 09:51:43 -0800

    Reorganizing

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'; //maybe remove
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './pages';
+import About from './pages/about';
+import Signup from './pages/signup';
+import Signin from './pages/login';
+import Create from './pages/createAuction'
+
+function App() {
+  return (
+    <Router>
+          <Navbar />
+          <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} /> 
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Signin} />
+              <Route path="/createAuction" component={Create} />
+           </Switch>
+    </Router>
+  );
+}
+
+export default App;
