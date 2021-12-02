9a24b21094991f3612d4ee2a2e78dc1274870224
daniel-adam-ce <danieladamce@gmail.com>
2021-12-02 12:46:24 -0800

    Bidding history fix

--- server/models/auction.js
+++ server/models/auction.js
@@ -35,23 +35,27 @@ const auctionSchema = mongoose.Schema({
         default: null,
         required: true,
     },
-    highestBidder: {
-        type: Object,
-        of: {
-            userBidding: {
-                type: mongoose.Schema.ObjectId,
-                default: null,
-                required: true,
-            },
-            bidAmount: {
-                type: Number,
-                default: 0,
-                required: true,
-            },
-            default: {}
-        },
+    // highestBidder: {
+    //     type: Object,
+    //     of: {
+    //         userBidding: {
+    //             type: mongoose.Schema.ObjectId,
+    //             default: null,
+    //             required: true,
+    //         },
+    //         bidAmount: {
+    //             type: Number,
+    //             default: 0,
+    //             required: true,
+    //         },
+    //         default: {}
+    //     },
+    //     default: {
+    //         userBidding: '',
+    //         bidAmount: 0
+    //     }
         
-    },
+    // },
     biddingHistory: {
         type: Array,
         of: {
