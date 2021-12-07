219068aebcabe9b0a8bc33c7bd2adf477a26bde1
Brian Irineo <irineobrian17@gmail.com>
2021-12-06 16:58:48 -0800

    Fixed bug on logout page that doesn't update user profile info

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -53,15 +53,31 @@ const Signin = () => {
 				})
 	};
 	
+	const [userr, setUserr] = useState({
+		profilePicture: '',
+		displayName: '',
+	})
+
+	const getUserupdateinfo = async () => {
+		try {axios.get(`https://bdh-server.herokuapp.com/user/get?id=${user._id}`)
+				.then((response) => {
+					console.log(response.data[0])
+					setUserr(response.data[0])
+					})} catch(err) {console.log(err)}
+				}
+
+	useEffect(() => {
+		getUserupdateinfo();
+	}, []);
+			
+
 	if (user){
-		//window.location = '/';
 		return (
 			<div className='log'>
-				<img src={user.profilePicture} class="profilepic"></img> 
-		
+				<img src={userr.profilePicture} class="profilepic"></img> 
 				<div className='LogoutBody'>
 					<div className="Logouttext">
-						{user.displayName} is logged in
+						{userr.displayName} is logged in
 					</div>
 					<div className="LogoutButton">
 						<button onClick = {handleLogout}>logout</button>
